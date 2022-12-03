a7f8ea10a05f8092a942c9185d98adcfdf389606
inventivetalent <mail@inventivetalent.org>
2022-12-03 12:41:09 +0100

    day 3 part 2

--- day3.py
+++ day3.py
@@ -14,19 +14,35 @@ def charid(c: str) -> int:
     return cid - lowercase_offset + 1
 
 
-sum = 0
-for rucksack in inp:
-    comp1, comp2 = rucksack[:len(rucksack) // 2], rucksack[len(rucksack) // 2:]
-    common = 0
-    for c1 in comp1:
-        for c2 in comp2:
-            if c1 == c2:
-                common = charid(c1)
-                break
-        else:  # TIL this is a thing https://stackoverflow.com/a/654002/6257838
-            continue  # only executed if the inner loop did NOT break
-        break  # only executed if the inner loop DID break
-    print(common)
-    sum += common
+def part1():
+    sum = 0
+    for rucksack in inp:
+        comp1, comp2 = rucksack[:len(rucksack) // 2], rucksack[len(rucksack) // 2:]
+        common = 0
+        for c1 in comp1:
+            for c2 in comp2:
+                if c1 == c2:
+                    common = charid(c1)
+                    break
+            else:  # TIL this is a thing https://stackoverflow.com/a/654002/6257838
+                continue  # only executed if the inner loop did NOT break
+            break  # only executed if the inner loop DID break
+        print(common)
+        sum += common
 
-print(sum)
+    print(sum)
+
+
+def part2():
+    sum = 0
+    for i in range(0, len(inp), 3):
+        a = set(inp[i])
+        b = set(inp[i + 1])
+        c = set(inp[i + 2])
+        x = a & b & c  # this is very neat.
+        print(x)
+        sum += charid(x.pop())
+    print(sum)
+
+
+part2()
